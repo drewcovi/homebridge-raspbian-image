112a8608af233eacb4d9b41b938ffb13359b1a1b
drewcovi <drew.covi@me.com>
2020-08-24 18:57:48 -0500

    making golang a local variable

--- stage3_homebridge/07-chromium-x11/00-run.sh
+++ stage3_homebridge/07-chromium-x11/00-run.sh
@@ -23,7 +23,7 @@ on_chroot << EOF
 set -x 
 
 # Install Golang
-export GOLANG="$(curl -k https://golang.org/dl/|grep armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
+GOLANG="$(curl -k https://golang.org/dl/|grep armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
 wget https://golang.org/dl/$GOLANG
 sudo tar -C /usr/local -xzf $GOLANG
 rm $GOLANG
