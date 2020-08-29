fe01f7840b1607307df28e3aadbdaf3262a080a1
drewcovi <drew.covi@me.com>
2020-08-29 15:19:05 -0500

    adding /root/go directory

--- stage3_homebridge/07-chromium-x11/00-run.sh
+++ stage3_homebridge/07-chromium-x11/00-run.sh
@@ -11,6 +11,7 @@ install -m 644 files/kiosk.service "${ROOTFS_DIR}/etc/systemd/system/"
 # install -v -d "${ROOTFS_DIR}/usr/local/share/kiosk"
 install -v -d "${ROOTFS_DIR}/usr/src/app/settings"
 install -v -d "${ROOTFS_DIR}/usr/local/lib/kiosk"
+install -v -d "${ROOTFS_DIR}/root/go"
 
 install -v -m 644 files/45-evdev.conf "${ROOTFS_DIR}/usr/share/X11/xorg.conf.d/"
 # udev rule to set specific permissions 
